b3e056511b475493cfbcd29912374fea7a185969
yanghui <1278719866@qq.com>
2019-09-11 18:19:12 +0800

    添加新增用户功能

--- src/pages/user/manage.js
+++ src/pages/user/manage.js
@@ -9,33 +9,51 @@ import { connect } from 'dva';
 import { relative } from 'path';
 import request from '../../utils/request';
 import ReactDOM from 'react-dom';
-import {Table,Pagination, Button,Dialog,Form,Input,Select } from 'element-react';
+import {Table,Pagination, Button,Dialog,Form,Input,Message } from 'element-react';
 import 'element-theme-default';
-
-// const columns = [
-//   { title: '用户Id', dataIndex: 'id' },
-//   { title: '用户名', dataIndex: 'username' },
-//   { title: '用户邮箱', dataIndex: 'email' },
-//   { title: '创建时间', dataIndex: 'created_at' },
-//   { title: '操作',render: (text, record) => (
-//     <span>
-//       <a>修改</a>
-//       <Divider type="vertical" />
-//       <a >删除</a>
-//     </span>
-//   ), },
-// ];
 
 class UserManage extends Component {
   constructor(props) {
     super(props);
   
     this.state = {
-      dialogVisible2: false,
       dialogVisible3: false,
       form: {
         name: '',
-        region: ''
+        email: '',
+        password:''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { validator: (rule, value, callback) => {
+            if (value === '') {
+              callback(new Error('请输入用户名'));
+            } else {
+              callback();
+            }
+          } }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: (rule, value, callback) => {
+            if (value === '') {
+              callback(new Error('请输入邮箱'));
+            } else {
+              callback();
+            }
+          } }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { validator: (rule, value, callback) => {
+            if (value === '') {
+              callback(new Error('请输入密码'));
+            } else {
+              callback();
+            }
+          } }
+        ],
       },
       columns: [
         {label: "用户Id",prop: "id",width:'100%'},
@@ -52,24 +70,60 @@ class UserManage extends Component {
           )
         }}
       ],
+      open() {
+        Message({
+          message: '添加成功',
+          type: 'success'
+        });
+      }
       // onClick={this.deleteRow.bind(this, index)}
     }
   }
-  deleteRow(index) {
-    const { data } = this.state;
-    data.splice(index, 1);
-    this.setState({
-      data: [...data]
-    })
-  }
+  //删除用户
   deleteUser(row) {
-    console.log(row);
+    // console.log(row);  //得到row是当前行用户的数据对象
     console.log(this.props.userList);
     let id=row.id;
     request.delete(`/api/v1/users/${id}`).then(response => {
       // console.log(response.data);
       this.props.getUserList();
-      // this.props.userList.splice(id, 1);
+    });
+  }
+  onChange(key, value) {
+    this.setState({
+      form: Object.assign({}, this.state.form, { [key]: value })
+    });
+
+  }
+  //表单提交时添加用户
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.refs.form.validate((valid) => {
+      if (valid) {
+        // alert('submit!');
+      } else {
+        console.log('error submit!!');
+        return false;
+      }
+    });
+    this.setState({ dialogVisible3: false });
+
+    // console.log(this.state.form.name)
+    // console.log(this.state.form.email)
+    request.post(`/api/v1/signup`,{
+      username:this.state.form.name,
+      email:this.state.form.email,
+      password:this.state.form.password,
+    })
+    .then(response => {
+      alert('添加成功!');
+      console.log(response);
+    })
+    .catch(error => {
+      alert('添加失败!');
+      console.log(error);
+      window.location.reload();
     });
   }
 
@@ -84,19 +138,22 @@ class UserManage extends Component {
           onCancel={ () => this.setState({ dialogVisible3: false }) }
         >
           <Dialog.Body>
-            <Form model={this.state.form}>
-              <Form.Item label="用户名" labelWidth="100">
-                <Input value={this.state.form.name}></Input>
+            <Form ref="form" model={this.state.form} rules={this.state.rules} className="demo-ruleForm">
+              <Form.Item label="用户名" prop="name" labelWidth="100">
+                <Input value={this.state.form.name} onChange={this.onChange.bind(this, 'name')}></Input>
               </Form.Item>
-              <Form.Item label="邮  箱" labelWidth="100">
-                <Input value={this.state.form.name}></Input>
+              <Form.Item label="邮  箱" prop="email" labelWidth="100">
+                <Input value={this.state.form.email} onChange={this.onChange.bind(this, 'email')}></Input>
+              </Form.Item>
+              <Form.Item label="密  码" prop="password" labelWidth="100">
+                <Input value={this.state.form.password} onChange={this.onChange.bind(this, 'password')}></Input>
               </Form.Item>
             </Form>
           </Dialog.Body>
 
           <Dialog.Footer className="dialog-footer">
             <Button onClick={ () => this.setState({ dialogVisible3: false }) }>取 消</Button>
-            <Button type="primary" onClick={ () => this.setState({ dialogVisible3: false }) }>确 定</Button>
+            <Button type="primary"  onClick={this.handleSubmit.bind(this)}>确 定</Button>
           </Dialog.Footer>
         </Dialog>
 
@@ -112,8 +169,6 @@ class UserManage extends Component {
           data={this.props.userList}
           highlightCurrentRow={true}
         />
-
-
       </div>
     );
   }
